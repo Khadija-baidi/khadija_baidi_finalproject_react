eff1494db360b4f23021b1cf93ce965c6baeea9e
khadija-baidi <baidikhadija3@gmail.com>
2025-06-03 16:09:56 +0100

    blog-p

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home.jsx';
 import Shop from './pages/shop.jsx';
 import Sale from './pages/sale.jsx';
+import Blog from './pages/blog.jsx';
 import Nav from './layouts/nav.jsx';
 import Footer from './layouts/footer.jsx';
 
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/sale" element={<Sale />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
       <Footer />
     </>
